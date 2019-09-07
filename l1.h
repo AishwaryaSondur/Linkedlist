7b30c4d965c797da57869f39568b5d6e4fd1d5b4
AishwaryaSondur <55013638+AishwaryaSondur@users.noreply.github.com>
2019-09-07 10:22:56 -0700

    Update l1.h

--- l1.h
+++ l1.h
@@ -9,34 +9,33 @@ typedef struct _slist_ Slist;
 
 
 struct _node_{
-int32_t data;       // node def(data and next node address)
+int32_t data;      
 Node *next;
 };
 
 
 struct _slist_{
 Node *head;
-Node *tail;           //head and tail data
+Node *tail;           
 uint32_t length;
 };
 
 
 
-/* public interface for user*/
 Slist slist_new();
 uint32_t slist_length(Slist *list);
 uint32_t slist_lookup(Slist *list,int32_t key);
 Slist* slist_add_head(Slist *list,int32_t element);
 Slist* slist_add_tail(Slist *list,int32_t element);
-//Slist* slist_delete_head(Slist *list);
-//Slist* slist_delete_tail(Slist *list);
+Slist* slist_delete_head(Slist *list);
+Slist* slist_delete_tail(Slist *list);
 Slist* slist_display(Slist *list);
-//Slist* slist_min_max(Slist *list);
-//Slist* slist_spec_ele(Slist *list, int32_t element,int32_t spec_ele);
-//Slist* slist_spec_ele_delete(Slist *list, int32_t spec_ele);
-//uint32_t list_compare(Slist *list1,Slist *list2);
+Slist* slist_min_max(Slist *list);
+Slist* slist_spec_ele(Slist *list, int32_t element,int32_t spec_ele);
+Slist* slist_spec_ele_delete(Slist *list, int32_t spec_ele);
+uint32_t list_compare(Slist *list1,Slist *list2);
 Slist* reverse_list(Slist *list);
-//Slist* union_twolist(Slist *union_list,Slist *list1,Slist *list2);
+Slist* union_twolist(Slist *union_list,Slist *list1,Slist *list2);
 Slist* intersection_twolist(Slist *intersection_list,Slist *list1,Slist *list2);
-//Slist* unique_slist(Slist* list,uint32_t element);
-#endif // SLIST_H_INCLUDED
+Slist* unique_slist(Slist* list,uint32_t element);
+#endif 
